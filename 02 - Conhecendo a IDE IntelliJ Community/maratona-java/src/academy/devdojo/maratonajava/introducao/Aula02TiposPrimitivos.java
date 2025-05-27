ed8f6e757fc5b9b32fe1a8caf92cbc0adc8faa40
purpesy <lucasgabrieldossantos753@gmail.com>
2025-05-27 17:52:33 -0300

    Fim da aula 2, entendo um pouco sobre variavel no java

--- 02 - Conhecendo a IDE IntelliJ Community/maratona-java/src/academy/devdojo/maratonajava/introducao/Aula02TiposPrimitivos.java
+++ 02 - Conhecendo a IDE IntelliJ Community/maratona-java/src/academy/devdojo/maratonajava/introducao/Aula02TiposPrimitivos.java
@@ -1,5 +1,11 @@
 package academy.devdojo.maratonajava.introducao;
 
 public class Aula02TiposPrimitivos {
-    
+    public static void main(String[] args) {
+        // int, double, float, char, byte, short, long, boolean
+        int n1 = 10;
+        int n2 = 15;
+        int resultado = n1 + n2;
+        System.out.println("A soma do numero 1 + o numero 2 é: " + resultado);
+    }
 }
