4b9185f503a2f04f2f6adac35c01566eefa50bfd
purpesy <lucasgabrieldossantos753@gmail.com>
2025-05-27 17:41:33 -0300

    Fim da primeira aula e inicio da 2 aula de java

---
+++ 02 - Conhecendo a IDE IntelliJ Community/maratona-java/src/academy/devdojo/maratonajava/introducao/Aula01OlaDevDojo.java
@@ -0,0 +1,8 @@
+package academy.devdojo.maratonajava.introducao;
+
+public class Aula01OlaDevDojo {
+    public static void main(String[] args){
+        // usar as 2 barras para fazer comentario!!!!!!!!!!!
+        System.out.println("Hello World!");
+    }
+}
